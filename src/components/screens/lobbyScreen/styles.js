ccf28c4fcb5e67b7a9fecbfc6eaa80397676bf99
tamirGer <tamir.gershberg@neuralegion.com>
2021-03-29 01:02:40 +0300

    fix(styles):fixed wrong export which caused night mode to not work

--- src/components/screens/lobbyScreen/styles.js
+++ src/components/screens/lobbyScreen/styles.js
@@ -1,24 +1,22 @@
-const styles = () => {
-  return {
-    night: {
-      backgroundColor: "#111",
-      color: "royalBlue",
-    },
-    day: {
-      backgroundColor: "#f3f3f3",
-    },
-    headerStyle: {
-      minHeight: "7vh",
-    },
-    dead: {
-      backgroundColor: "rgba(240,240,240,0.6)",
-      color: "gray",
-    },
-    alive: {
-      color: "white",
-      backgroundColor: "rgba(43,96,222,0.6)",
-    },
-  };
+const styles = {
+  night: {
+    backgroundColor: "#111",
+    color: "royalBlue",
+  },
+  day: {
+    backgroundColor: "#f3f3f3",
+  },
+  headerStyle: {
+    minHeight: "7vh",
+  },
+  dead: {
+    backgroundColor: "rgba(240,240,240,0.6)",
+    color: "gray",
+  },
+  alive: {
+    color: "white",
+    backgroundColor: "rgba(43,96,222,0.6)",
+  },
 };
 
 export default styles;
